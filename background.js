4900c31a6a6517b5fcfd685b5746575e1492df02
David Merrick <david@david-merrick.com>
2015-04-08 07:10:25 -0700

    Moved setting headers into a function

--- background.js
+++ background.js
@@ -5,25 +5,38 @@ var requestFilter = {
 extraInfoSpec = ['requestHeaders', 'blocking'],
 handler = function(details) {
 
-var isUserAgentSet = false;
+var USER_AGENT_HEADER = 'User-Agent';
 var IOS_USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25";
+
 
 var headers = details.requestHeaders,
     blockingResponse = {};
 
-for (var i = 0, l = headers.length; i < l; ++i) {
-    if (headers[i].name == 'User-Agent') {
-        headers[i].value = IOS_USER_AGENT;
-        isUserAgentSet = true;
-	break;
+function isHeaderSet(headerName){
+    for (var i = 0, l = headers.length; i < l; ++i) {
+        if (headers[i].name == headerName) {
+            return true;
+        }
     }
+    return false;
 }
-
-if (!isUserAgentSet) {
+    
+function setHeader(headerName, value){
+    for (var i = 0, l = headers.length; i < l; ++i) {
+        if (headers[i].name == headerName) {
+            headers[i].value = value;
+            return;
+        }
+    }   
+}
+    
+if (!isHeaderSet(USER_AGENT_HEADER)) {
     headers.push({
-        name: "User-Agent",
+        name: USER_AGENT_HEADER,
         value: IOS_USER_AGENT
     });
+} else {
+    setHeader(USER_AGENT_HEADER, IOS_USER_AGENT);
 }
 
 blockingResponse.requestHeaders = headers;
